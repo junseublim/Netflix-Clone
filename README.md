2029128e7cbf4d75e05d5598360e72fbf1bedf21
2016026480 <junslim11@gmail.com>
2021-03-15 00:41:18 +0900

    git init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Netflix Clone
+
+리액트를 활용하여 만든 넷플릭스 클론 웹사이트입니다. 영화 데이터는 tmdb api를 사용하였습니다.
+
+## URL
+
+[Netflix Clone](https://netflix-clone-de391.web.app/)
+
+firebase를 사용하여 배포하였습니다.
+
+## 향후 추가
+
+- 회원가입, 로그인
+- 영상 삽입
+- 마우스 hover 시 영상 정보, 트레일러 보여주기
+
+## 사용한 라이브러리
+
+- react
+- react-youtube
+- movie-trailer
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <h1 className="footer-item">Email: junslim11@gmail.com</h1>
+      <h1 className="footer-item">github: https://github.com/junseublim</h1>
+      <h1 className="footer-item">
+        Based on youtube video :
+        https://www.youtube.com/watch?v=XtMThy8QKqU&t=6069s
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
